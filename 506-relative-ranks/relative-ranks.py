ec34ffe32e9a88a4760269bb45a89cdaa9144f72
nishanthking20 <127777074+nishanthking20@users.noreply.github.com>
2024-05-08 19:32:48 +0530

    Time: 68 ms (47.32%) | Memory: 17.8 MB (34.48%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        score_map = {score[i]: i for i in range(len(score))}
+
+        # Sort the scores in descending order
+        score.sort(reverse=True)
+
+        # Create a result list to store the ranks
+        result = ["" for _ in range(len(score))]
+        for i in range(len(score)):
+            # Assign ranks based on the position of each score in the sorted list
+            if i == 0:
+                result[score_map[score[i]]] = "Gold Medal"
+            elif i == 1:
+                result[score_map[score[i]]] = "Silver Medal"
+            elif i == 2:
+                result[score_map[score[i]]] = "Bronze Medal"
+            else:
+                result[score_map[score[i]]] = str(i + 1)
+
+        return result
